72475ad72b39c495313e2a2c9a2d80ee84b8ac93
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2020-07-09 22:45:34 -0400

    Added support for multple graphs, WIP.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
+#include <algorithm>
 #include <fstream>
+#include <iostream>
 #include <vector>
 #include <GLFW/glfw3.h>
 #include "utils.hpp"
 typedef long long lli;
-std::vector<lli>gr;
+std::vector<std::vector<lli>>gr;
 lli curr;
 lli fr;
+lli t;
+lli longest;
 
 void render()
 {
@@ -15,32 +19,38 @@ void render()
 
 	if(curr)
 	{
-		float big = gr[curr - 1];
+		float big = 0;
 
-		for(lli i = 0; i < curr; i++)
+		for(lli line=0;line<gr.size();line++)
 		{
-			DrawLine(x, y, x + diff, big == 0 ? -0.75 : -0.75 + gr[i] / big);
-			x += diff;
-			y = -0.75 + (big == 0 ? 0 : gr[i] / big);
+			big = gr[line][curr - 1];
+			for(lli i = 0; i < curr; i++)
+			{
+				DrawLine(x, y, x + diff, big == 0 ? -0.75 : -0.75 + gr[line][i] / big);
+				x += diff;
+				y = -0.75 + (big == 0 ? 0 : gr[line][i] / big);
 
-			if(curr < 20 || i == curr - 1)
-			{
-				DrawCircle(x, y, 0.01);
-			}
-			else if(curr < 50 && i % 2 == 0)
-			{
-				DrawCircle(x, y, 0.01);
-			}
-			else if(curr < 200 && i % 6 == 0)
-			{
-				DrawCircle(x, y, 0.01);
+				if(curr < 20 || i == curr - 1)
+				{
+					DrawCircle(x, y, 0.01);
+				}
+				else if(curr < 50 && i % 2 == 0)
+				{
+					DrawCircle(x, y, 0.01);
+				}
+				else if(curr < 200 && i % 6 == 0)
+				{
+					DrawCircle(x, y, 0.01);
+				}
 			}
 		}
+
 	}
 
-	if(curr < gr.size() - 1 && fr % 15 == 0)
+	if(curr < longest - 1 && fr % (t / (curr + 1)) == 0)
 	{
 		++curr;
+		fr = 0;
 	}
 
 	++fr;
@@ -55,6 +65,7 @@ int main(int argl, char**argv)
 		{
 			std::ifstream ifs ("data.txt");
 			lli cnt = 0;
+			lli len = 0;
 			lli tmp = 0;
 			ifs >> cnt;
 
@@ -62,12 +73,20 @@ int main(int argl, char**argv)
 
 			for(lli i = 0; i < cnt; i++)
 			{
-				ifs >> tmp;
-				gr.push_back(tmp);
+				ifs >> len;
+				longest = std::max(longest,len);
+				gr.emplace_back();
+				gr.back().reserve(len);
+				for(lli j=0;j<len;j++)
+				{
+					ifs >> tmp;
+					gr.back().push_back(tmp);
+				}
 			}
 
 			ifs.close();
 			glfwMakeContextCurrent(window);
+			t = 240;
 
 			double last = glfwGetTime();
 			double now = 0;
